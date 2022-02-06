bf5c4cc409b99c76d5c01d91dff7931623037224
Hirekari-Abhishek-Raj <abhishekhirekaari@gmail.com>
2022-02-06 09:13:50 +0530

    newone

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,16 +1,27 @@
-var txt = document.querySelector("input");
-var btn = document.querySelector("button");
-var list = document.querySelector('.todos');
-var count = 0;
-btn.addEventListener('click', function () {
-    var data = txt.value;
-    var para = document.createElement("p");
-    para.setAttribute("key", count);
-    para.append(data);
-    list.append(para);
-    txt.value = "";
-    count = count + 1
-    para.addEventListener('click', function () {
-        para.remove()
+var btn = document.querySelector("button")
+var btn1 = document.querySelector("#f")
+fetch('https://jsonplaceholder.typicode.com/todos').then(function (data) {
+    console.log(data)
+    return data.json()
+}).then(function (data) {
+    btn.addEventListener('click', function () {
+        for (i = 0; i < 100; i++) {
+            if (data[i].completed === true) {
+                const para = document.createElement('p')
+                const out = data[i].title
+                para.textContent = out
+                document.getElementById("root").append(para)
+            }
+        }
+    })
+    btn1.addEventListener('click', function () {
+        for (i = 0; i < 100; i++) {
+            if (data[i].completed === false) {
+                const para = document.createElement('p')
+                const out = data[i].title
+                para.textContent = out
+                document.getElementById("root").append(para)
+            }
+        }
     })
 })
